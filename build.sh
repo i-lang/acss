e183ee7cc801152b9b14a3c8af59eb373176fee7
KellyYang <housemeow@gmail.com>
2016-08-07 16:33:16 +0800

    show less compile time

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
-echo "start to compile..."
+printf "start to compile..."
+
+start_time=$(date +%s)
+
 lessc src/less/main.less dist/css/acss.css
 lessc --clean-css src/less/main.less dist/css/acss.min.css
-echo "less compiled."
+
+end_time=$(date +%s)
+
+diff_time=$[ $end_time - $start_time]
+printf "less has compiled. It cost %d seconds" $diff_time
