05772de90bfa8a9d9058e77a81336fe6479cfbcf
KellyYang <housemeow@gmail.com>
2016-09-04 15:11:09 +0800

    sass script

--- build-sass.sh
+++ build-sass.sh
@@ -1,2 +1,12 @@
 cd src/
+
+printf "start to compile sass..."
+
+start_time=$(date +%s)
+
 compass compile -c prod_config.rb --force
+
+end_time=$(date +%s)
+
+diff_time=$[ $end_time - $start_time]
+printf "sass has compiled. It cost %d seconds" $diff_time
